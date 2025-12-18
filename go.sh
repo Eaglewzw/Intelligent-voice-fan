943ba3220b057fdf3133eb8d7035073f4d02ed7c
Eaglewzw <1460853569@qq.com>
2025-12-18 12:57:31 +0800

    优化结构

--- go.sh
+++
@@ -1,9 +0,0 @@
-git init
-git add .
-git remote add origin https://github.com/Eaglewzw/Intelligent-voice-fan.git
-git config user.email "1460853569@qq.com"
-git config user.name "Eaglewzw"
-git config user.password "12345wzw"
-git push -u origin master
-git pull origin master
-git status
